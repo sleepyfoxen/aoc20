1a825349d1e3e71436fd38d0ea66e135343e6cff
fox <fox@tardis.ed.ac.uk>
2020-12-14 13:10:05 +0000

    aaaaaaaaaaAAAAAAAAAAAAA

--- day13.py
+++ day13.py
@@ -4,85 +4,37 @@ import numpy as np
 with open('inputs/day13', 'r') as f:
     inp = f.read().strip().splitlines()
 
-# inp = '''939
-# 7,13,x,x,59,x,31,19'''.strip().splitlines()
-
-# inp = '''0
-# 17,x,13,19'''.strip().splitlines()
-
-# inp = '''0
-# 1789,37,47,1889'''.strip().splitlines()
-
 t, busses = inp
 t = int(t)
 
 soonest = 1e10
 best_bus = None
 for bus in busses.split(','):
-    if bus == 'x':
-        continue
-
+    if bus == 'x': continue
     bus = int(bus)
     next_bus = math.ceil(t / bus) * bus
+
     if next_bus < soonest:
         soonest = next_bus
         best_bus = bus
 
-
-print(best_bus, soonest, soonest - t)
 print((soonest - t) * best_bus)
 
 constraints = []
 
 for td, bus in enumerate(busses.split(',')):
-    if bus == 'x':
-        # print('no restrictions on t+{}'.format(td))
-        continue
-
-    # print('bus {} must depart at t+{}'.format(bus, td))
+    if bus == 'x': continue
     constraints.append((int(bus), td))
 
-print(constraints)
-
-# for bus, td in constraints:
-#     print('0 = k+{} mod {},'.format(td, bus))
-
-print(next(zip(*constraints)))
 N = np.lcm.reduce(next(zip(*constraints)))
-# print(N)
 s = r = 0
 for bus, td in constraints:
     # 0 = k + td, mod bus
-
-    # calculate bi
     Ni = N/bus
     Ni = int(Ni)
     bi = pow(Ni, -1, bus)
 
-    # print(bi, Ni, bi * Ni, td)
     r += td * bi * Ni
     s += bi * Ni
 
-# print(s, r)
 print((r - N - 2 * (r * s % N)) % N)
-
-# inc = i = constraints[0][0]
-# while True:
-#     for bus, td in constraints:
-#         print(i, td, bus, (i + td) % int(bus))
-#         if not (i + td) % int(bus) == 0:
-#             break
-#     else:
-#         print(i)
-#         break
-
-#     # if all(i % (int(bus) + td) == 0 for bus, td in constraints):
-#     #     break
-#     i += inc
-    
-
-# print(i)
-
-# 35 = x (mod 41)
-# 41 = x (mod 35)
-# 
--- day14.py
+++ day14.py
@@ -3,13 +3,6 @@ from collections import defaultdict
 with open('inputs/day14', 'r') as f:
     inp = f.read().strip().splitlines()
 
-# inp = '''mask = 000000000000000000000000000000X1001X
-# mem[42] = 100
-# mask = 00000000000000000000000000000000XXXX
-# mem[26] = 1
-# mem[100] = 1
-# '''.strip().splitlines()
-
 
 mem = defaultdict(int)
 mask = None
@@ -21,26 +14,18 @@ for line in inp:
 
     val = int(val)
     addr = int(cmd[4:].split(']')[0])
-    # print(addr, val, bin(val))
-    
     masked_val = [ None for _ in mask ]
     for i, (vb, mb) in enumerate(zip(format(val, '036b')[::-1], mask[::-1])):
         if mb == 'X':
             masked_val[-1-i] = vb
-        
         elif mb == '1':
             masked_val[-1-i] = '1'
-        
         else:
             masked_val[-1-i] = '0'
-    
 
-    # print(''.join(masked_val))
-    # print(mem)
     mem[addr] = int(''.join(masked_val), 2)
     
 print(sum(mem.values()))
-
 
 mem = defaultdict(int)
 mask = None
@@ -51,27 +36,19 @@ for line in inp:
         continue
 
     val = int(val)
-    addr = int(cmd[4:].split(']')[0])
-    print(addr, val, bin(val))
-    
+    addr = int(cmd[4:].split(']')[0])    
     masked_val = [ None for _ in mask ]
     for i, (vb, mb) in enumerate(zip(format(addr, '036b')[::-1], mask[::-1])):
         if mb == 'X':
             masked_val[-1-i] = 'X'
-        
         elif mb == '1':
             masked_val[-1-i] = '1'
-        
         elif mb == '0':
             masked_val[-1-i] = vb
     
-
-    print(''.join(masked_val))
     mask_untainted = ''.join(masked_val)
-    highest_mask = ''.join(masked_val)
     masks = []
     masks_n = 2**mask_untainted.count('X')
-    # print(masks_n)
     for i in range(masks_n):
         eep = format(i, '0{}b'.format(mask_untainted.count('X')))
         mask_working = ''.join(masked_val)
@@ -83,64 +60,8 @@ for line in inp:
                     cnt += 1
         masks.append(mask_working)
 
-    print()
-    if len(masks) != masks_n:
-        print('aaaaaaaaaaaaaaaaaaaaaa')
-        raise
-    # for mask in masks: print(mask)
-
     for mask_ in masks:
-        print(mask_, int(mask_, 2), val)
         mem[int(mask_, 2)] = val
 
-    print('\n\n')
 
-    # while 'X' in lowest_mask:
-    #     ll, x, lr = lowest_mask.partition('X')
-    #     rl, x, rr = highest_mask.partition('X')
-    #     # if r == '':
-    #     #     masks.add()
-    #     masks.add(ll + '0' + lr.replace('X', '0'))
-    #     masks.add(ll + '1' + lr.replace('X', '1'))
-    #     masks.add(rl + '0' + lr.replace('X', '0'))
-    #     masks.add(rl + '1' + lr.replace('X', '1'))
-    #     masks.add(rl.replace('X', '0') + '0' + rr.replace('X', '0'))
-    #     masks.add(rl.replace('X', '1') + '1' + rr.replace('X', '1'))
-    #     masks.add(ll.replace('X', '0') + '0' + lr.replace('X', '0'))
-    #     masks.add(ll.replace('X', '1') + '1' + lr.replace('X', '1'))
-    #     lowest_mask = lowest_mask.replace('X', '0', 1)
-    #     highest_mask = highest_mask.replace('X', '1', 1)
-
-    #     if '0000000000000000000000000000001' in masks:
-    #         print('???')
-        # highest_mask = highest_mask.replace('X', '1', 1)
-        # if rr == '':
-        #     masks.add(highest_mask)
-        #     masks.add(lowest_mask)
-
-    # rbits = set()
-    # while 'X' in lowest_mask:
-
-
-    # for c in lowest_mask[::-1]:
-    #     # print(c)
-    #     if c == 'X':
-    #         l, x, r = lowest_mask.partition('X')
-    #         masks.add(l + '0' + r)
-    #         masks.add(l + '1' + r)
-
-
-    # print(masks, len(masks))
-
-    # mem[addr] = int(''.join(masked_val), 2)
-    
 print(sum(mem.values()))
-
-
-# def awoo(m: str, ms=set()):
-#     while 'X' in m:
-#         l, x, r = m.partition('X')
-#         return ms | awoo(l + '0' + r) | awoo(l + '1' + r)
-    
-#     ms.add(m)
-#     return ms
