a8930ae58061a7b1f454213293b6b18ba8ee29eb
fox <fox@tardis.ed.ac.uk>
2020-12-17 19:54:13 +0000

    more code from the underworld (ii)

--- day17.py
+++ day17.py
@@ -3,13 +3,13 @@ from itertools import product
 with open('inputs/day17', 'r') as f:
     inp = f.read().strip().splitlines()
 
-ones = list()
+ones = set()
 
 origin_delta = None
 for x, row in enumerate(inp):
     for y, bit in enumerate(row):
         if bit == '#':
-            ones.append((x, y, 0, 0))
+            ones.add((x, y, 0, 0))
 
 asdf = list(product((-1, 0, 1), repeat=4))
 
@@ -18,19 +18,17 @@ def f(ones):
         ones_next = set()
         for v in ones:
             for dx, dy, dz, dw in asdf:
-                bk, counter = False, 0
+                counter = 0
                 was_one = dx == dy == dz == dw == 0
                 nn = (v[0] + dx, v[1] + dy, v[2] + dz, v[3] + dw)
                 for dx, dy, dz, dw in asdf:
                     if dx == dy == dz == dw == 0: continue
                     adj = (nn[0] + dx, nn[1] + dy, nn[2] + dz, nn[3] + dw)
                     if adj in ones: counter += 1
-                    if counter > 3:
-                        bk = True
-                        break
-                if bk: continue
-                if counter == 3 or (was_one and (2 <= counter <= 3)): 
-                    ones_next.add(nn)
+                    if counter > 3: break
+                else:
+                    if counter == 3 or (was_one and counter in (2, 3)):
+                        ones_next.add(nn)
 
         ones = ones_next
         print(len(ones_next))
